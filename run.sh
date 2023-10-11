7e1e395bee377d847a94a099013156edfbeb627a
hujiande <hujiande@megvii.com>
2023-10-11 19:40:36 +0800

    feat：启动脚本-需要先执行如下创建db的sql语句

--- run.sh
+++ run.sh
@@ -16,6 +16,8 @@ then
 kill -9 $pid
 fi
 
+# 需要先执行如下创建db的sql语句
+# CREATE DATABASE `oneapi` CHARACTER SET utf8 COLLATE utf8_general_ci;
 export SQL_DSN="root:Aa112211@tcp(localhost:3306)/oneapi?charset=utf8&parseTime=True&loc=Local&timeout=10s&readTimeout=30s&writeTimeout=60s"
 
 nohup ./one-api --port=3001 &
