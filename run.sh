ff3c9d3da4181c4b88bbd23a1ef90574c2aec3d6
hujiande <hujiande@megvii.com>
2023-10-11 18:15:03 +0800

    feat：启动脚本-修改为mysql启动方式

--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -ex
 
-cd web
-yarn build
-cd ..
+if [[ "$1" == "" ]]
+then
+  cd web
+  yarn build
+  cd ..
+fi
 
 go build -o one-api
 
@@ -12,6 +15,8 @@ if [[ "$pid" != "" ]]
 then 
 kill -9 $pid
 fi
+
+export SQL_DSN="root:Aa112211@tcp(localhost:3306)/oneapi?charset=utf8&parseTime=True&loc=Local&timeout=10s&readTimeout=30s&writeTimeout=60s"
 
 nohup ./one-api --port=3001 &
 
